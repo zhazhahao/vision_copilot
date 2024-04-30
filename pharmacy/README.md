fa35b6470deb3aac6e15529d7821013b7847254a
Geniukx <Geniukx@outlook.com>
2024-04-30 14:54:00 +0800

    20240430 update

---
+++ pharmacy/README.md
@@ -0,0 +1,14 @@
+# Benchmark
+
+## Metrics
+
+### Object Detection
+Accuracy, precision, recall.
+
+### Action Recognition
+#### Qualitative Evaluation 
+* 检出率：一个动作正在发生，且被识别的百分比。
+* 分均误报：一个动作没有发生，但被识别到了，平均每分钟发生的次数。
+
+#### Quantitative Evaluation
+* IoU@0.5
--- pharmacy/export.py
+++
@@ -1,6 +0,0 @@
-from qinglang.data_structure.video.video_toolbox import VideoToolbox
-from qinglang.data_structure.image.image_toolbox import ImageToolbox, ImageFlow
-
-
-itb = ImageToolbox(ImageFlow("/home/portable-00/VisionCopilot/pharmacy/work_dirs/20240426-150401/images"))
-itb.to_video()
